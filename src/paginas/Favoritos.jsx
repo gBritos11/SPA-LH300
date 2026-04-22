7eaccfa5dcc7d13d07bf5dc5f5205005bb926571
PaulaViggiano <paula.viggiano@est.fi.uncoma.edu.ar>
2026-04-22 16:19:04 -0300

    Creacion de componentes, esqueleto

---
+++ src/paginas/Favoritos.jsx
@@ -0,0 +1,9 @@
+export const Favoritos = () => {
+
+    return (
+        <div className="bg-white p-8">
+            <h1 className="text-3xl font-bold">Página de Favoritos</h1>
+        </div>
+    );
+
+}
---
+++ src/paginas/Inicio.jsx
@@ -0,0 +1,10 @@
+
+export const Inicio = () => {
+
+    return (
+        <div className="bg-white p-8">
+            <h1 className="text-3xl font-bold">Página de Inicio</h1>
+        </div>
+    );
+
+}
