a9f475417139116cf0f26fd7a27b697c574ac73c
PaulaViggiano <paula.viggiano@est.fi.uncoma.edu.ar>
2026-04-26 17:34:53 -0300

    Realice la tarea de navegacion del navLink

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from "react-router-dom";
-import BarraNavegacion from "./componentes/BarraNavegacion";
+import { BarraNavegacion } from "./componentes/BarraNavegacion";
+import Pie from "./componentes/Pie";
 import Inicio from './paginas/Inicio';
 import Detalles from "./paginas/Detalles";
 import Favoritos from "./paginas/Favoritos";
 import Error from "./paginas/Error";
-import Pie from "./componentes/Pie";
+
 
 
 const App = () => {
--- src/componentes/BarraNavegacion.jsx
+++ src/componentes/BarraNavegacion.jsx
@@ -1,27 +1,30 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
-const BarraNavegacion = () => {
+//Funcion para estilos 
+const estiloNavLink = ({isActive}) => 
+    isActive ? 'text-blue-600 font-semibold border-b-2 border-blue-600 pb-1'
+    : 'text-slate-600 hover:text-slate-900 transition-colors';
+
+export const BarraNavegacion = () => {
     return (
-        <nav className="sticky top-0 z-10 w-full bg-grey shadow-md">
-            <div className="max-w-6xl mx-auto px-6 py-4 flex justify-between items-center">
-                <Link to="/" className="flex items-center gap-2" >
-                 {/* <img src="/logo.png" alt="Logo" className="h-8" /> */}
+        <nav className="sticky top-0 z-10 w-full bg-white shadow-md">
+            <div className="max-w-6xl mx-auto px-6 py-4 flex justify-between intems-center">
+                <NavLink to="/" className="flex items-center gap-2">
                     <span className="text-xl font-bold text-slate-800">
                         Destinos
                     </span>
-                </Link>
-
+                </NavLink>
                 <div className="flex items-center gap-6">
-                    <Link to="/" className="text-slate-600 hoover:text-slate-900 transition-colors"> 
+                    <NavLink to="/" className={estiloNavLink} end>
                         Inicio
-                    </Link>
-                    <Link to="/" className="text-slate-600 hoover:text-slate-900 transition-colors">
+                    </NavLink>
+                    <NavLink to="/favoritos" className={estiloNavLink}>
                         Favoritos
-                    </Link>
+                    </NavLink>
+
                 </div>
             </div>
         </nav>
     );
 }
 
-export default BarraNavegacion;
