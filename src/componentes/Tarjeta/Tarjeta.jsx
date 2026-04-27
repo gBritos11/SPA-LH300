1c11cc745df59a4bcd0f1afb3d9b96f0a781325a
gBritos11 <gabriel.britos.epet20@gmail.com>
2026-04-27 18:03:47 -0300

    Se corrigio el componente tarjeta para pasar el destino como objeto y no desestructurado

--- src/componentes/Tarjeta/Tarjeta.jsx
+++ src/componentes/Tarjeta/Tarjeta.jsx
@@ -2,25 +2,35 @@ import Boton from '../Boton/Boton'; // Asegúrate de que la ruta sea la correcta
 import { ArrowRight } from 'lucide-react'; // Importamos un icono para darle nivel
 import estilo from './Tarjeta.module.css';
 
-const Tarjeta = ({ imagen, titulo, descripcion, presupuesto, action }) => { 
+const Tarjeta = ({ destino, action }) => { 
+  if (!destino) return null;
+
+  const { 
+    nombre = "Destino desconocido", 
+    presupuesto = 0, 
+    imagen = "", 
+    pais = "No especificado", 
+    descripcion = "Sin descripción disponible" 
+  } = destino;
+
   return (
-    <div className="max-w-sm bg-stone-100 rounded-xl shadow-md overflow-hidden hover:shadow-xl transition-all duration-300 flex flex-col">
+    <div className="max-w-sm bg-stone-100 rounded-xl shadow-md overflow-hidden hover:shadow-xl transition-all duration-300 flex flex-comax-w-sm bg-stone-100 rounded-xl shadow-md overflow-hidden hover:shadow-xl transition-all duration-300 flex flex-col">
       
       <div className="relative">
         <img 
           src={imagen} 
-          alt={titulo} 
+          alt={nombre} 
           className="w-full h-48 object-cover" 
         />
       </div>
 
       <div className="p-4 flex flex-col flex-1 text-center">
         <h2 className="text-lg font-semibold text-gray-800 line-clamp-1 h-7 hover:text-xl transition-all duration-300">
-          {titulo}
+          {nombre}
         </h2>
         
         <p className="text-gray-600 mt-2 line-clamp-2 text-sm h-10">
-          {descripcion}
+          {pais} - {descripcion}
         </p>
         
         <p className="text-600 font-bold mt-4 text-xl">
--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -21,19 +21,16 @@ const Inicio = () => {
 
     return (
         <div className="p-8">
-            <h1 className="text-3xl font-bold">Destinos</h1>
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-                    {destinos.map((destino) => (
+            <h1 className="text-3xl font-bold mb-8">Destinos</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+                {destinos.map((destino) => (
                     <Tarjeta
                         key={destino.id}
-                        imagen={destino.imagen}
-                        titulo={destino.nombre}
-                        descripcion={destino.descripcion}
-                        presupuesto={destino.presupuesto}
+                        destino={destino} 
                         action={() => navigate(`/destino/${destino.id}`)}
                     />
-                     ))}
-                </div>
+                ))}
+            </div>
         </div>   
     );
 }
