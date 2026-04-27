685d140ec3b1ea890550f0767559738688b57c8b
gBritos11 <gabriel.britos.epet20@gmail.com>
2026-04-27 18:17:45 -0300

    correcciones

--- src/componentes/Tarjeta/Tarjeta.jsx
+++ src/componentes/Tarjeta/Tarjeta.jsx
@@ -3,11 +3,15 @@ import { ArrowRight } from 'lucide-react'; // Importamos un icono para darle niv
 import estilo from './Tarjeta.module.css';
 
 const Tarjeta = ({ destino, action }) => { 
+  if (!destino) return null;
 
-  const imagen = destino.imagen || 'https://picsum.photos/400/300';
-  const titulo = destino.titulo || 'Sin nombre';
-  const descripcion = destino.descripcion || 'Sin descripcion disponible';
-  const presupuesto = destino.presupuesto || 0;
+  const { 
+    imagen = 'https://picsum.photos/400/300',
+    titulo = 'Sin nombre',
+    descripcion = 'Sin descripción disponible',
+    presupuesto = 0,
+    pais = 'desconocido'
+  } = destino;
   
   return (
     <div className="max-w-sm bg-stone-100 rounded-xl shadow-md overflow-hidden hover:shadow-xl transition-all duration-300 flex flex-comax-w-sm bg-stone-100 rounded-xl shadow-md overflow-hidden hover:shadow-xl transition-all duration-300 flex flex-col">
@@ -15,14 +19,14 @@ const Tarjeta = ({ destino, action }) => {
       <div className="relative">
         <img 
           src={imagen} 
-          alt={nombre} 
+          alt={titulo} 
           className="w-full h-48 object-cover" 
         />
       </div>
 
       <div className="p-4 flex flex-col flex-1 text-center">
         <h2 className="text-lg font-semibold text-gray-800 line-clamp-1 h-7 hover:text-xl transition-all duration-300">
-          {nombre}
+          {titulo}
         </h2>
         
         <p className="text-gray-600 mt-2 line-clamp-2 text-sm h-10">
