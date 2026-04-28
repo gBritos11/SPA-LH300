d5e0fd10d05524764be6d86cfda4d33c636bcfa8
PaulaViggiano <paula.viggiano@est.fi.uncoma.edu.ar>
2026-04-28 09:37:36 -0300

    Realice la barra de busqueda destino, paises, cumpliendo con los requerimientos solicitados

--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -1,13 +1,18 @@
+import { useState } from 'react';
 import  useDestinos  from '../hooks/useDestinos';
 import  Tarjeta from './../componentes/Tarjeta/Tarjeta'
 import { useNavigate } from "react-router-dom";
-import MensajesApp from "./../componentes/MensajesApp/MensajesApp"
+import MensajesApp from "../componentes/MensajesApp/MensajesApp"
 import Boton from '../componentes/Boton/Boton';
-
+import Busqueda from '../componentes/Busqueda/Busqueda';
 const Inicio = () => {
 
     const { destinos, loading, error } = useDestinos();
     const navigate = useNavigate();
+
+    //Agrego el buscador
+    const [filtro, setFiltro] = useState('');
+    console.log('filtro actual:', filtro);
 
     if (loading) return <MensajesApp tipo="cargando" mensaje="Buscando destinos..." />;
 
@@ -17,13 +22,35 @@ const Inicio = () => {
         </MensajesApp>
     );
 
-    if (destinos.length === 0) return <MensajesApp tipo="vacio" mensaje="Parece que no hay viajes disponibles." />;
+    if (destinos.length === 0) return <MensajesApp tipo="vacio" mensaje="No hay resultados para esa búsqueda." />;
+
+    const terminoBusqueda = filtro.toLowerCase();
+    const destinosFiltrados = destinos.filter((destino) => {
+        if(!terminoBusqueda) return true;
+
+        const coincideNombre = (destino.nombre || '').toLowerCase().includes(terminoBusqueda);
+        const coincidePais = (destino.pais || '').toLowerCase().includes(terminoBusqueda);
+        const coincideDescripcion = (destino.descripcion || '').toLowerCase().includes(terminoBusqueda);
+
+        return coincideNombre || coincidePais || coincideDescripcion;
+    })
 
     return (
         <div className="p-8">
             <h1 className="text-3xl font-bold">Destinos</h1>
+                <div className='mb-8 flex justify-center'>
+                    <Busqueda valor={filtro} onChange={setFiltro} />
+                </div>
+
+                {destinosFiltrados.length === 0 ? (
+                    <MensajesApp 
+                        tipo="vacio"
+                        mensaje={`No encontramos destinos para "${filtro}"`}
+                    />
+                ) : (
+                
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-                    {destinos.map((destino) => (
+                    {destinosFiltrados.map((destino) => (
                     <Tarjeta
                         key={destino.id}
                         imagen={destino.imagen}
@@ -34,6 +61,7 @@ const Inicio = () => {
                     />
                      ))}
                 </div>
+                )}
         </div>   
     );
 }
